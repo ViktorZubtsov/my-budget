a56fb29426fc184b0646226838c7b4478a56f11e
Viktor Zubtsov <v.zubtsov@userstory.ru>
2024-02-26 23:03:53 +0700

    test

--- src/modules/Settings/components/MyAccounts/index.tsx
+++ src/modules/Settings/components/MyAccounts/index.tsx
@@ -5,7 +5,7 @@ import {memo} from 'react';
 import {AccountsBadge} from '@/components/AccountsBadge';
 import {IAccount} from '@/model';
 
-import {DATA_TEST_ID_ACCOUNT, MAX_ACCOUNT_LENGTH} from '../../constants';
+import {MAX_ACCOUNT_LENGTH} from '../../constants';
 import {EmptyText, Header, MyAccountsStyled} from './styled';
 
 interface IMyAccountsProps {
@@ -22,7 +22,7 @@ export const MyAccounts = memo<IMyAccountsProps>(({accountsList}) => {
                     {accountsList.length}&nbsp;/&nbsp;{MAX_ACCOUNT_LENGTH}
                 </TextM>
             </Header>
-            <MyAccountsStyled id={DATA_TEST_ID_ACCOUNT.LIST}>
+            <MyAccountsStyled>
                 {!accountsList?.length && <EmptyText>У вас нет счетов</EmptyText>}
                 {accountsList && accountsList.map(({colorCode, id, name}) => <AccountsBadge key={id} $colorCode={colorCode} text={name} size="l" />)}
             </MyAccountsStyled>
