1016b3bcc7b1927e4794dcb57b0d6ce55ac4932e
Viktor Zubtsov <v.zubtsov@dev.userstory.ru>
2024-03-01 23:28:33 +0700

    MY-21

--- src/modules/Settings/components/Sound/index.tsx
+++ src/modules/Settings/components/Sound/index.tsx
@@ -1,6 +1,6 @@
 import {IconVolumeOff, IconVolumeUp} from '@salutejs/plasma-icons';
 import {Switch} from '@salutejs/plasma-ui';
-import React, {ChangeEventHandler, memo, useEffect, useState} from 'react';
+import React, {ChangeEventHandler, memo, useCallback, useEffect, useState} from 'react';
 
 import {LOCAL_STORAGE_KEYS} from '@/constant';
 
@@ -13,27 +13,27 @@ interface ISoundSettingProps {
 export const SoundSetting = memo<ISoundSettingProps>(({isChecked = false}) => {
     const [checked, setChecked] = useState<boolean>(isChecked);
 
-    useEffect(() => {
-        setChecked(isChecked);
-    }, [isChecked]);
-
     const handleSwitch: ChangeEventHandler<HTMLInputElement> = (e) => {
         setChecked(e.target.checked);
         return localStorage.setItem(LOCAL_STORAGE_KEYS.VIBRATION, String(e.target.checked));
     };
-
-    useEffect(() => {
-        if (!Object.keys(localStorage).includes(LOCAL_STORAGE_KEYS.VIBRATION)) {
-            setChecked(true);
-            localStorage.setItem(LOCAL_STORAGE_KEYS.VIBRATION, String('true'));
-        }
-    }, []);
     return (
         <CardStyled p="10x">
             <Content>
-                <Title>Вибрация {checked ? <IconVolumeUp size="s" color="inherit" /> : <IconVolumeOff size="s" color="inherit" />}</Title>
+                <Title>
+                    Вибрация{' '}
+                    {checked ? (
+                        <div data-testid={'soundSettingOn'}>
+                            <IconVolumeUp size="s" color="inherit" />
+                        </div>
+                    ) : (
+                        <div data-testid={'soundSettingOff'}>
+                            <IconVolumeOff size="s" color="inherit" />
+                        </div>
+                    )}
+                </Title>
 
-                <Switch id="sound-setting-switch" label="Вибрация на события" checked={checked} onChange={handleSwitch} />
+                <Switch data-testid={'soundSettingSwitch'} label="Вибрация на события" checked={checked} onChange={handleSwitch} />
             </Content>
         </CardStyled>
     );
