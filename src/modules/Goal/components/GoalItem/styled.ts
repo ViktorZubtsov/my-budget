b8e915345fbc2997c2e1237392e90b9d523beaf7
Viktor Zubtsov <v.zubtsov@dev.userstory.ru>
2024-03-09 20:16:41 +0700

    commit

--- src/modules/Goal/components/GoalItem/styled.ts
+++ src/modules/Goal/components/GoalItem/styled.ts
@@ -11,7 +11,7 @@ export const GoalItemStyled = styled.div<SpacingProps>`
     width: 100%;
     background-color: rgba(255, 255, 255, 0.1);
     backdrop-filter: blur(10px);
-    border-radius: 4px;
+    border-radius: 15px;
     ${applySpacing}
 `;
 
