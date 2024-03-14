8d4dc874ea760f0e692f899c8d44b5f89b644ca5
Viktor Zubtsov <v.zubtsov@dev.userstory.ru>
2024-03-14 14:09:10 +0700

    commit

--- src/modules/Goal/components/GoalItem/Index.tsx
+++ src/modules/Goal/components/GoalItem/Index.tsx
@@ -1,4 +1,6 @@
-import {useRouter} from 'next/navigation';
+'use client';
+
+import {useRouter} from 'next/router';
 import React, {memo} from 'react';
 import {SwipeableList, SwipeableListItem} from 'react-swipeable-list';
 
