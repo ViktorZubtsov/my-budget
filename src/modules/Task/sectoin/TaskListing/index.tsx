755cd16c684b3d0d3baa71984b882772cd1159ae
Viktor Zubtsov <v.zubtsov@dev.userstory.ru>
2024-03-20 12:54:19 +0700

    feat(emptylist): add EmptyList

--- src/modules/Task/sectoin/TaskListing/index.tsx
+++ src/modules/Task/sectoin/TaskListing/index.tsx
@@ -1,6 +1,7 @@
 import {memo, useMemo, useState} from 'react';
 
 import {TaskSum} from '@/modules/Task/components/TaskSum';
+import {GoalXListStyled} from '@/modules/Task/sectoin/TaskListing/styled';
 
 import {AddButton} from '../../../../components/AddButton';
 import {EmptyList} from '../../../../components/EmptyList';
@@ -11,7 +12,6 @@ import {TaskItem} from '../../../Goal/components/TaskItem';
 import {AddTask} from '../../components/AddTask';
 import {EditTask} from '../../components/EditTask';
 import {useTask} from '../../hooks';
-import styles from './styles.module.scss';
 
 export const TaskListing = memo(({taskList, accountsList}: {taskList: TTask[]; accountsList: IAccount[]}) => {
     const {isProcessLoader} = useLoaderStore();
@@ -44,7 +44,7 @@ export const TaskListing = memo(({taskList, accountsList}: {taskList: TTask[]; a
         <>
             {Boolean(sum) && <TaskSum sum={sum} />}
 
-            <div className={styles.GoalXList}>
+            <GoalXListStyled>
                 {!taskList.length && <EmptyList text="Список задач пуст" />}
                 {Boolean(taskList.length) &&
                     taskList.map((task) => (
@@ -61,7 +61,7 @@ export const TaskListing = memo(({taskList, accountsList}: {taskList: TTask[]; a
                             task={task}
                         />
                     ))}
-            </div>
+            </GoalXListStyled>
             <AddButton isFixed onClick={() => setIsShowAddTask(true)} />
             <EditTask
                 accountsList={accountsList}
