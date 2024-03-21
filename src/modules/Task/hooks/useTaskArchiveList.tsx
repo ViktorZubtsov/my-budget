0ff626346cce4b7ea04cf3ebbe97efac4f199698
Viktor Zubtsov <v.zubtsov@dev.userstory.ru>
2024-03-21 17:33:20 +0700

    commit

--- src/modules/Task/hooks/useTaskArchiveList.tsx
+++ src/modules/Task/hooks/useTaskArchiveList.tsx
@@ -8,14 +8,14 @@ import {GoalContext} from '@/modules/Goal/context';
 export const useTaskArchiveList = () => {
     const {id: goalId} = useContext(GoalContext);
     const {data} = useSWR<TTask[]>(getTaskArchiveListKey(goalId), {revalidateOnMount: false});
-    const taskArchiveList = data ?? [];
+    const taskArchiveList = useMemo(() => data ?? [], [data]);
 
     const sum = useMemo<number | undefined>(() => {
         return taskArchiveList?.reduce((previousValue, currentValue) => Number(previousValue) + Number(currentValue.price), 0);
     }, [taskArchiveList]);
 
     return {
-        taskArchiveList,
         sum,
+        taskArchiveList,
     };
 };
