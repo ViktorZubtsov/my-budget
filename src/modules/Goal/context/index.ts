e8e01469a3b38baeec4c8840007384ec416d4493
Viktor Zubtsov <v.zubtsov@dev.userstory.ru>
2024-03-21 17:35:56 +0700

    commit

--- src/modules/Goal/context/index.ts
+++ src/modules/Goal/context/index.ts
@@ -3,7 +3,7 @@ import {createContext} from 'react';
 import {IGoalShort} from '@/model';
 
 export const GoalContext = createContext<IGoalShort>({
-    id: '',
     description: '',
+    id: '',
     name: '',
 });
