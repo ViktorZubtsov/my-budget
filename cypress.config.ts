18da104d04ba2b1eeb583c2d683defc9f59ddc59
Viktor Zubtsov <v.zubtsov@dev.userstory.ru>
2024-03-23 22:49:34 +0700

    commit

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,8 @@ import {defineConfig} from 'cypress';
 
 export default defineConfig({
     e2e: {
-        baseUrl: 'https://my-budget-space.vercel.app',
-        projectId: "h1wj78",
+        baseUrl: 'http://localhost:3000',
+        projectId: 'h1wj78',
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
