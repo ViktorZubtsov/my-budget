988ac7b0f7a9fce0fe27f7f6df292a91eb621ce4
Viktor Zubtsov <58132336+ViktorZubtsov@users.noreply.github.com>
2024-03-23 22:25:31 +0700

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import {defineConfig} from 'cypress';
 export default defineConfig({
     e2e: {
         baseUrl: 'https://my-budget-space.vercel.app',
+        projectId: "h1wj78",
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
