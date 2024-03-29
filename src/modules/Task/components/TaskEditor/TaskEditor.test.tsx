5f15067d727e8fcec30d3c29e42df370bf8bd17c
Viktor Zubtsov <v.zubtsov@userstory.ru>
2024-03-29 20:57:24 +0700

    commit

--- src/modules/Task/components/TaskEditor/TaskEditor.test.tsx
+++ src/modules/Task/components/TaskEditor/TaskEditor.test.tsx
@@ -5,7 +5,10 @@ import {describe, expect, test, vi} from 'vitest';
 import {TEST_ID_ACCOUNT, TEST_ID_ADD_TASK} from '@/constant/dataTest';
 import {generateAccount, generateWord, ResizeObserver} from '@/helpers';
 import {MOCK_ACCOUNT, MOCK_TASK} from '@/mock';
+import {ACCOUNTS_OPTIONS} from '@/modules/Settings/components/AddAccount/constants';
 import {TaskEditor} from '@/modules/Task/components/TaskEditor/index';
+
+const ACCOUNT = ACCOUNTS_OPTIONS[0];
 
 const DEFAULT_TASK = {
     bankAccount: MOCK_TASK['bankAccount'],
@@ -13,6 +16,7 @@ const DEFAULT_TASK = {
     price: MOCK_TASK['price'],
 };
 export const SAVE_TEXT = 'Сохранить';
+export const ADD_TASK_TEXT = 'Добавить';
 export const NAME = 'NAME';
 export const PRICE = 500;
 
@@ -21,7 +25,7 @@ describe('TaskEditor', () => {
     global.IS_REACT_ACT_ENVIRONMENT = true;
     global.ResizeObserver = ResizeObserver;
 
-    const {getByTestId, rerender} = render(
+    const {getByTestId, getByText, rerender} = render(
         <TaskEditor
             accountsList={[]}
             isOpen={true}
@@ -38,6 +42,8 @@ describe('TaskEditor', () => {
     const nameField = getByTestId(TEST_ID_ADD_TASK.NAME) as HTMLInputElement;
     const priceField = getByTestId(TEST_ID_ADD_TASK.PRICE) as HTMLInputElement;
     const button = getByTestId(TEST_ID_ADD_TASK.SUBMIT);
+    const selectItem = getByText(ACCOUNT.text);
+    const select = getByTestId(TEST_ID_ACCOUNT.SELECT);
 
     test('Check Edit mode', async () => {
         const handleClick = vi.fn();
@@ -71,6 +77,44 @@ describe('TaskEditor', () => {
 
         expect(handleClick).toHaveBeenCalledWith({
             bankAccount: DEFAULT_TASK.bankAccount,
+            name: NAME,
+            price: PRICE,
+        });
+    });
+
+    test('Create task', async () => {
+        const handleClick = vi.fn();
+
+        rerender(
+            <TaskEditor
+                accountsList={[MOCK_ACCOUNT]}
+                isOpen={true}
+                onClose={() => {}}
+                title={''}
+                isFetching={false}
+                isAccountsFetching={false}
+                addAccount={() => {}}
+                isEdit={false}
+                onSubmit={handleClick}
+            />
+        );
+
+        expect(nameField.value).toBe('');
+        expect(priceField.value).toBe('0');
+        expect(button.textContent).toBe(ADD_TASK_TEXT);
+
+        fireEvent.change(nameField, {target: {value: NAME}});
+        fireEvent.change(priceField, {target: {value: PRICE}});
+        fireEvent.change(priceField, {target: {value: PRICE}});
+        fireEvent.change(select, {target: {value: PRICE}});
+        // fireEvent.click(selectItem);
+
+        await act(async () => {
+            fireEvent.click(button);
+        });
+
+        expect(handleClick).toHaveBeenCalledWith({
+            bankAccount: '',
             name: NAME,
             price: PRICE,
         });
