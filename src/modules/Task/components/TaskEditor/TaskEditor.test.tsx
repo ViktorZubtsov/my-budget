f3e80a742a9b06b560606324ed7ca189d6224263
Viktor Zubtsov <v.zubtsov@userstory.ru>
2024-04-01 18:45:37 +0700

    commit

--- src/modules/Task/components/TaskEditor/TaskEditor.test.tsx
+++ src/modules/Task/components/TaskEditor/TaskEditor.test.tsx
@@ -1,14 +1,14 @@
-import {fireEvent, render} from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import {act} from 'react-dom/test-utils';
 import {describe, expect, test, vi} from 'vitest';
 
 import {TEST_ID_ACCOUNT, TEST_ID_ADD_TASK} from '@/constant/dataTest';
 import {generateAccount, generateWord, ResizeObserver} from '@/helpers';
-import {MOCK_ACCOUNT, MOCK_TASK} from '@/mock';
+import {MOCK_ACCOUNT, MOCK_ACCOUNT_LIST, MOCK_ACCOUNT_TWO, MOCK_TASK} from '@/mock';
 import {ACCOUNTS_OPTIONS} from '@/modules/Settings/components/AddAccount/constants';
 import {TaskEditor} from '@/modules/Task/components/TaskEditor/index';
 
-const ACCOUNT = ACCOUNTS_OPTIONS[0];
+const ACCOUNT = MOCK_TASK.name;
 
 const DEFAULT_TASK = {
     bankAccount: MOCK_TASK['bankAccount'],
@@ -25,7 +25,7 @@ describe('TaskEditor', () => {
     global.IS_REACT_ACT_ENVIRONMENT = true;
     global.ResizeObserver = ResizeObserver;
 
-    const {getByTestId, getByText, rerender} = render(
+    const {rerender} = render(
         <TaskEditor
             accountsList={[]}
             isOpen={true}
@@ -39,17 +39,16 @@ describe('TaskEditor', () => {
         />
     );
 
-    const nameField = getByTestId(TEST_ID_ADD_TASK.NAME) as HTMLInputElement;
-    const priceField = getByTestId(TEST_ID_ADD_TASK.PRICE) as HTMLInputElement;
-    const button = getByTestId(TEST_ID_ADD_TASK.SUBMIT);
-    const selectItem = getByText(ACCOUNT.text);
-    const select = getByTestId(TEST_ID_ACCOUNT.SELECT);
+    const nameField = screen.getByTestId(TEST_ID_ADD_TASK.NAME) as HTMLInputElement;
+    const priceField = screen.getByTestId(TEST_ID_ADD_TASK.PRICE) as HTMLInputElement;
+    const button = screen.getByTestId(TEST_ID_ADD_TASK.SUBMIT);
 
     test('Check Edit mode', async () => {
         const handleClick = vi.fn();
+
         rerender(
             <TaskEditor
-                accountsList={[MOCK_ACCOUNT]}
+                accountsList={MOCK_ACCOUNT_LIST}
                 defaultValues={DEFAULT_TASK}
                 isOpen={true}
                 onClose={() => {}}
@@ -61,7 +60,8 @@ describe('TaskEditor', () => {
                 onSubmit={handleClick}
             />
         );
-        const accountField = getByTestId(TEST_ID_ACCOUNT.SELECT);
+        const accountField = screen.getByTestId(TEST_ID_ACCOUNT.SELECT);
+        const selectItem = screen.getByText(MOCK_ACCOUNT_TWO.name);
 
         expect(nameField.value).toBe(DEFAULT_TASK.name);
         expect(priceField.value).toBe(String(DEFAULT_TASK.price));
@@ -70,13 +70,14 @@ describe('TaskEditor', () => {
 
         fireEvent.change(nameField, {target: {value: NAME}});
         fireEvent.change(priceField, {target: {value: PRICE}});
+        fireEvent.click(selectItem);
 
         await act(async () => {
             fireEvent.click(button);
         });
 
         expect(handleClick).toHaveBeenCalledWith({
-            bankAccount: DEFAULT_TASK.bankAccount,
+            bankAccount: MOCK_ACCOUNT_TWO.id,
             name: NAME,
             price: PRICE,
         });
@@ -87,7 +88,7 @@ describe('TaskEditor', () => {
 
         rerender(
             <TaskEditor
-                accountsList={[MOCK_ACCOUNT]}
+                accountsList={MOCK_ACCOUNT_LIST}
                 isOpen={true}
                 onClose={() => {}}
                 title={''}
@@ -98,23 +99,24 @@ describe('TaskEditor', () => {
                 onSubmit={handleClick}
             />
         );
+        const accountField = screen.getByTestId(TEST_ID_ACCOUNT.SELECT);
+        const selectItem = screen.getByText(MOCK_ACCOUNT_TWO.name);
 
         expect(nameField.value).toBe('');
         expect(priceField.value).toBe('0');
+        expect(accountField.textContent).toBe('Выберите счет');
         expect(button.textContent).toBe(ADD_TASK_TEXT);
 
         fireEvent.change(nameField, {target: {value: NAME}});
         fireEvent.change(priceField, {target: {value: PRICE}});
-        fireEvent.change(priceField, {target: {value: PRICE}});
-        fireEvent.change(select, {target: {value: PRICE}});
-        // fireEvent.click(selectItem);
+        fireEvent.click(selectItem);
 
         await act(async () => {
             fireEvent.click(button);
         });
 
         expect(handleClick).toHaveBeenCalledWith({
-            bankAccount: '',
+            bankAccount: MOCK_ACCOUNT_TWO.id,
             name: NAME,
             price: PRICE,
         });
