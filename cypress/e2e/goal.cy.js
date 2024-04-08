967288e6f9c2122b25656cbd5765b1bc4a915de8
Viktor Zubtsov <v.zubtsov@userstory.ru>
2024-04-08 14:02:05 +0700

    commit

--- cypress/e2e/goal.cy.js
+++ cypress/e2e/goal.cy.js
@@ -1,4 +1,4 @@
-import {TEST_ID_GOAL, TEST_ID_GOAL_ITEM} from '../../src/constant/dataTest';
+import {TEST_ID_COMMON, TEST_ID_GOAL, TEST_ID_GOAL_ITEM} from '../../src/constant/dataTest';
 import {KEY_GOAL_LIST} from '../../src/core/SWRKeys';
 import {random} from '../../src/helpers/random';
 
@@ -8,10 +8,10 @@ const url = '/';
 const goalUrl = `${KEY_GOAL_LIST}?uid=*`;
 const hostname = 'localhost';
 const goalItemSelector = `[data-testid="${TEST_ID_GOAL.LISTING}"] [data-testid="${TEST_ID_GOAL_ITEM.GOAL_ITEM}"]`;
-// const goalItemButton = `[data-testid="${TEST_ID_COMMON.REMOVE_BUTTON}"]`;
+const goalItemButton = `[data-testid="${TEST_ID_COMMON.REMOVE_BUTTON}"]`;
 
 describe('Gaol Page', () => {
-    it('Create goal', () => {
+    it('CRUD goal', () => {
         cy.intercept({
             hostname,
             method: 'POST',
@@ -22,14 +22,14 @@ describe('Gaol Page', () => {
         cy.get(`#${TEST_ID_GOAL.GOAL_NAME_INPUT}`).focus().type(testValue).should('have.value', testValue);
         cy.get(`#${TEST_ID_GOAL.GOAL_SUBMIT}`).click();
         cy.wait('@addGoal');
-        // cy.get(goalItemSelector)
-        //     .last()
-        //     .should('have.text', testValue)
-        //     .trigger('mousedown', 'right')
-        //     .trigger('mousemove', {clientX: 500})
-        //     .trigger('mouseup');
-        // cy.wait(1000);
-        // cy.get(goalItemButton).last().click();
+        cy.get(goalItemSelector)
+            .last()
+            .should('have.text', testValue)
+            .trigger('mousedown', 'right')
+            .trigger('mousemove', {clientX: 600})
+            .trigger('mouseup');
+        cy.wait(1000);
+        cy.get(goalItemButton).last().click();
         cy.get(goalItemSelector).last().should('have.text', testValue);
     });
 });
